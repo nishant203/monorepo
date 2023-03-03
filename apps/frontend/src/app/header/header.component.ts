044c96b8cfe43cbf4412aff95bdde15176771bd6
Nishant Varshney <nishant203@gmail.com>
2023-03-03 16:53:50 +0530

    add new component (#2)

---
+++ apps/frontend/src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-angular-nest-monorepo-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent {}
